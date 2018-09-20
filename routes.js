b553bf14c95af417c425e24bfe78c11b9ed6521c
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-09-20 17:12:04 +0200

    updated routes

--- routes.js
+++ routes.js
@@ -1,80 +1,73 @@
-module.exports =  function (regNumbers) {
+module.exports =  function () {
 
                     async function show (req,res) {
 
                         try {
-                            let normalList = await regNumbers.getPlate();
-                            let displayRegs = normalList.reverse();
-
-                            let drop_down = await regNumbers.dropDown();
-                            res.render('home', {
-                                displayRegs,
-                                drop_down
-                            });
+                            res.render('home');
                         } catch (error) {
                             res.redirect("/");
                         }
 
                     };
 
-                    async function showAdd (req, res, next) {
-                        try {
-                            let {
-                                regValue
-                            } = req.body;
-                            let result = await regNumbers.enterRegPlate(regValue);
+                    // async function showAdd (req, res, next) {
+                    //     try {
+                    //         let {
+                    //             regValue
+                    //         } = req.body;
+                    //         let result = await regNumbers.enterRegPlate(regValue);
 
-                            if (result.success === true) {
-                                req.flash('info', result.message);
-                                res.redirect("/");
-                            } else if (result.success === false) {
+                    //         if (result.success === true) {
+                    //             req.flash('info', result.message);
+                    //             res.redirect("/");
+                    //         } else if (result.success === false) {
 
-                                req.flash('error', result.message);
-                                res.redirect("/");
-                            }
+                    //             req.flash('error', result.message);
+                    //             res.redirect("/");
+                    //         }
 
 
-                        } catch (error) {
-                            next(error);
-                        }
-                    };
+                    //     } catch (error) {
+                    //         next(error);
+                    //     }
+                    // };
                     
-                    async function filter (req, res)
-                    {
-                        try {
-                            let {
-                                townTag
-                            } = req.params;
-                            let displayRegs = await regNumbers.filterTown(townTag);
-                            let drop_down = await regNumbers.dropDown(townTag);
+                    // async function filter (req, res)
+                    // {
+                    //     try {
+                    //         let {
+                    //             townTag
+                    //         } = req.params;
+                    //         let displayRegs = await regNumbers.filterTown(townTag);
+                    //         let drop_down = await regNumbers.dropDown(townTag);
 
 
-                            res.render("home", {
-                                displayRegs,
-                                drop_down
-                            });
+                    //         res.render("home", {
+                    //             displayRegs,
+                    //             drop_down
+                    //         });
 
 
-                        } catch (error) {
-                            res.redirect("/");
-                        }
-                    };
+                    //     } catch (error) {
+                    //         res.redirect("/");
+                    //     }
+                    // };
 
-                    async function reset(req, res) {
-                        try {
-                            await regNumbers.resetDataBase();
-                            req.flash('info', 'The database has just been cleared!');
-                            res.redirect("/");
-                        } catch (error) {
-                            res.redirect("/");
-                        }
-                    };
+                    // async function reset(req, res) {
+                    //     try {
+                    //         await regNumbers.resetDataBase();
+                    //         req.flash('info', 'The database has just been cleared!');
+                    //         res.redirect("/");
+                    //     } catch (error) {
+                    //         res.redirect("/");
+                    //     }
+                    // };
 
 
                     return{
-                        show,
-                        showAdd,
-                        filter,
-                        reset
+                        show
+                        // showAdd,
+                        // filter,
+                        // reset
                     }
                 }
